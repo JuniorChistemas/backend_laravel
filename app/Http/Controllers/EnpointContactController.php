c787e714ac140d2804228875050dde1b34aae28b
junior15 <junior15062023@gmail.com>
2025-12-11 14:54:17 -0500

    insert customer

--- app/Http/Controllers/EnpointContactController.php
+++ app/Http/Controllers/EnpointContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\EnpointContact;
+use App\Models\Customer;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
@@ -22,6 +23,33 @@ class EnpointContactController extends Controller
         );
 
         $resultRecapcha = $googleResponse->json();
+
+        if ($resultRecapcha['tokenProperties']['action'] !== 'submit') {
+            return response()->json([
+                'status' => false,
+                'message' => 'Acción inválida detectada en reCAPTCHA.',
+            ], 422);
+        }
+
+        if ($resultRecapcha['riskAnalysis']['score'] < 0.5) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Actividad sospechosa detectada. Por favor, inténtelo de nuevo.',
+            ], 422);
+        }
+
+        // save customer 
+        $customer = Customer::create([
+            'first_name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'description' => $request->input('message'),
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Contacto recibido exitosamente.',
+            'customer_id' => $customer->id,
+        ], 201);
 
         Log::info('RECAPTCHA RESPONSE: ', $resultRecapcha);
     }
--- database/migrations/2025_11_21_163407_create_customers_table.php
+++ database/migrations/2025_11_21_163407_create_customers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('first_name', '100');
-            $table->string('last_name', '100');
+            $table->string('last_name', '100')->nullable();
             $table->string('email', '150')->unique();
             $table->char('phone', 9)->nullable();
             $table->text('description')->nullable();
